4d8af3a6934e0f08681ea5e7026b0bb102bc409d
ryotetora <surreal7295@gmail.com>
2020-10-10 01:33:02 +0900

    ログアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   # ログインしてなければログイン画面に飛ばす
   before_action :configure_permitted_parameters, if: :devise_controller?
 
