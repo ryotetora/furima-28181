49fca31a7f56520d10ae39b328d9511db1bdbadd
ryotetora <surreal7295@gmail.com>
2020-10-10 00:37:00 +0900

    ログイン状態により表示変化の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # ログインしてなければログイン画面に飛ばす
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    # デフォルトで使えないパラメータの許可
   end
 end
