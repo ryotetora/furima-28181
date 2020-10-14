214718be09f2dd15c88d838fb7bf26c0f0074174
ryotetora <surreal7295@gmail.com>
2020-10-14 23:11:20 +0900

    バリデーションの設定

--- db/migrate/20200928152922_devise_create_users.rb
+++ db/migrate/20200928152922_devise_create_users.rb
@@ -5,8 +5,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :nickname,           null: false
-      t.string :email,              null: false
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, unique: true
+      t.string :encrypted_password, null: false, default: "", unique: true
       t.string :first_name,         null: false
       t.string :last_name,          null: false
       t.string :first_name_kana,    null: false
