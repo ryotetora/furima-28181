6e30283d9b01f84c93a86c48e633bc5701024ed7
ryotetora <surreal7295@gmail.com>
2020-10-26 23:51:57 +0900

    price表示修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,14 +1,11 @@
 // 金額idをpriceInput変数に格納
-function item_price() {
+window.addEventListener('load', function(){
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
   console.log(inputValue);
 })
-}
-
-const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-// ここから手数料計算できず途中
-    
-window.addEventListener('load', item_price);
+})
+// const addTaxDom = document.getElementById("add-tax-price");
+//     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+// ここの計算の仕方が意味わからんあとできく
