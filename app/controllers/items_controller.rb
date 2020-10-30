cd8ec259d87a6c9e5d52d3a263d2357f0f028ba9
ryotetora <surreal7295@gmail.com>
2020-10-31 01:54:01 +0900

    ストロングパラメータにuser_id取得

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
+      # 保存に成功すればrootにとばす
     else
       render :new
     end
@@ -27,8 +28,8 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image,:user,:name,:description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_days_id,:price)
+    params.require(:item).permit(:image,:user,:name,:description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_days_id,:price).merge(user_id: current_user.id)
+    # ストロングパラメータで出品者、商品情報を取得
   end
-  # これあってるか自信薄
 
 end
