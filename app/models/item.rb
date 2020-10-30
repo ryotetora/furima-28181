f14e9577cc4f5808692730cc5281a848d3ea9e49
ryotetora <surreal7295@gmail.com>
2020-10-31 02:10:47 +0900

    エラーメッセージ設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,8 +16,8 @@ class Item < ApplicationRecord
   validates :image, :name, :description, :category, :condition, :delivery_charge, :prefecture, :shipping_days, :price, presence: true
     #空の投稿を保存できない
 
-  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_days_id, numericality: { other_than: 1 } 
-#選択が1の「--」の時は保存できない
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_days_id, numericality: { other_than: 1, message: "項目が未選択です" } 
+  #選択が1の「--」の時は保存できない
 
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
   # 数値のみ許可、入力可能最小値、最大値設定
