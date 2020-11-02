b5d0dc21ee72c1f8dbcfd64dbfba63e53d57eda8
ryotetora <surreal7295@gmail.com>
2020-11-02 11:52:01 +0900

    不要な記述の削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,13 +3,15 @@ window.addEventListener('load', function(){
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log(inputValue);
 
   const addTaxDom = document.getElementById("add-tax-price");
   const tax = Math.floor(inputValue / 10);
   addTaxDom.innerHTML = tax
+  // 手数料計算
+  
   const profit = document.getElementById("profit")
   profit.innerHTML = Math.floor(inputValue - tax);
+  // 利益計算
 })
 
 })
