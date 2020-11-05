f04ecdae840f3b30655c36a8fa4b6eac53796544
ryotetora <surreal7295@gmail.com>
2020-11-05 20:28:31 +0900

    アソシエーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :item
+  has_many :items
+  # itemモデルとのアソシエーション
+  
   #  各モジュール
   with_options presence: true do
     validates :nickname
