2e4d53d8ba14e74e8fecff9345bacbdd0b759fbe
ryotetora <surreal7295@gmail.com>
2020-11-10 15:46:18 +0900

    DB修正

--- db/migrate/20201110020620_create_addresses.rb
+++ db/migrate/20201110020620_create_addresses.rb
@@ -1,13 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-      t.references   :order,          null: false, foreign_key: true
+      t.references   :order,          foreign_key: true
       t.string       :post_code,      null: false
-      t.integer      :prefecture_id,  null: false, foreign_key: true
+      t.integer      :prefecture_id,  null: false
       t.string       :city,           null: false
       t.string       :house_number,   null: false
-      t.string       :building_name,
       t.string       :phone_number,   null: false
+      t.string       :building_name
       t.timestamps
     end
   end
