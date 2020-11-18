6dc7533e800036828b4e3fb284a209fac0822b70
ryotetora <surreal7295@gmail.com>
2020-11-18 20:43:46 +0900

    formオブジェクト作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,4 @@ class User < ApplicationRecord
   has_many :orders
   # 各モデルとのアソシエーション
 
-  #  各モジュール
-  with_options presence: true do
-    validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字・ひらがな・カタカナを使用して下さい' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字・ひらがな・カタカナを使用して下さい' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用して下さい' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用して下さい' }
-
-    validates :birthday
-  end
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字を使用して下さい' }
-  validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字を使用してください' }
 end
