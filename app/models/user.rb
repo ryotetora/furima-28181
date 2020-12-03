59ea82217b590dc837246aec7dabcaae96ff2afb
ryotetora <surreal7295@gmail.com>
2020-12-04 01:00:25 +0900

    userモデルのエラー文の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,13 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字・ひらがな・カタカナを使用して下さい' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字・ひらがな・カタカナを使用して下さい' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用して下さい' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用して下さい' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'には漢字・ひらがな・カタカナを使用して下さい' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'には漢字・ひらがな・カタカナを使用して下さい' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'にはカタカナを使用して下さい' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'にはカタカナを使用して下さい' }
     validates :birthday
   end
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字を使用して下さい' }
-  validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字を使用してください' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には半角英数字を使用して下さい' }
+  validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には半角英数字を使用してください' }
 end
