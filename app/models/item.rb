7de5b4facfafd49d001fec301dbe6de487b78337
ryotetora <surreal7295@gmail.com>
2020-12-03 16:15:43 +0900

    日本語変換に伴い、エラー分修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,14 +15,17 @@ class Item < ApplicationRecord
   has_one :order
   # 各モデルとのアソシエーション
 
-  validates :image, :name, :description, :category, :condition, :delivery_charge, :prefecture, :shipping_days, :price, presence: true
+  validates :image, presence: { message: 'を選択してください' }
+  # 画像未選択では保存できない
+
+  validates :name, :description, :category, :condition, :delivery_charge, :prefecture, :shipping_days, :price, presence: true
   # 空の投稿を保存できない
 
-  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_days_id, numericality: { other_than: 1, message: '項目が未選択です' }
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_days_id, numericality: { other_than: 1, message: 'の項目が未選択です' }
   # 選択が1の「--」の時は保存できない
 
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は300〜9,999,999の間で入力してください' }
   # 入力可能最小値、最大値設定
-  validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters.' }
+  validates :price, numericality: { only_integer: true, message: 'を半角数字で入力してください' }
   # 半角数値のみ許可
 end
