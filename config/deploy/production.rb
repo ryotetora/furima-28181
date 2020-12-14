72b922abcb7399fd253ca630d38bfc6327d739b9
ryotetora <surreal7295@gmail.com>
2020-12-14 23:45:18 +0900

    不要スペースの削除

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.250.112.148 ', user: 'ec2-user', roles: %w{app db web}
+server '54.250.112.148', user: 'ec2-user', roles: %w{app db web}
